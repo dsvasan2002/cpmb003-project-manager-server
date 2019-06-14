728a38dcca9700bb3330a2028ddc8f6297d64012
dsvasan2002 <dsvasan2002@gmail.com>
2019-06-13 21:31:21 -0400

    Fixed the error in adding task

--- model/task.js
+++ model/task.js
@@ -4,13 +4,14 @@ const autoIncrement= require('mongoose-sequence')(mongoose);
 var TaskSchema = new mongoose.Schema({
     taskId: {type: Number},
     taskName: {type: String, required: true, unique: true, dropDups: true},
-    parentId: {type: mongoose.Schema.ObjectId, ref: 'Task', required: false},
-    projectId: {type: mongoose.Schema.ObjectId, ref: 'Project', required: true},
+    parent: {type: mongoose.Schema.ObjectId, ref: 'Task', required: false},
+    project: {type: mongoose.Schema.ObjectId, ref: 'Project', required: true},
     startDate: { type: Date, default: Date.now },
     endDate: { type: Date, default: Date.now },
     priority: { type: Number, required: true, min: 0, max: 30 },
     hasFinished: { type: Boolean, default: false },
     User: {type: mongoose.Schema.Types.ObjectId, ref: 'User' }    
+
 })
 
 TaskSchema.plugin(autoIncrement, {inc_field: 'taskId'});
